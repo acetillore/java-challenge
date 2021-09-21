57fe58bc7d3a10c547cf3ed194f3cf012ab9cb5f
Glyn Estillore <glynestillore@gmail.com>
2021-09-21 21:09:08 +0900

    first commit

---
+++ src/test/java/jp/co/axa/apidemo/services/EmployeeServiceTest.java
@@ -0,0 +1,28 @@
+package jp.co.axa.apidemo.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiceTest {
+
+    @Test
+    public void retrieveEmployees() {
+    }
+
+    @Test
+    public void getEmployee() {
+    }
+
+    @Test
+    public void saveEmployee() {
+    }
+
+    @Test
+    public void deleteEmployee() {
+    }
+
+    @Test
+    public void updateEmployee() {
+    }
+}
